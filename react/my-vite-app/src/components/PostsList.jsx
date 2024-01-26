1a163888ef9ab3093f4a9fa4d650d4e9b9904ef7
yanabilous <yanabilous05@gmail.com>
2024-01-26 17:53:33 +0200

    Submitting Data with action()s

--- react/my-vite-app/src/components/PostsList.jsx
+++ react/my-vite-app/src/components/PostsList.jsx
@@ -7,22 +7,19 @@ function PostsList() {
   const posts = useLoaderData();
 
 
-  function addPostHandler(postData) {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
+  // function addPostHandler(postData) {
+  //   fetch("http://localhost:8080/posts", {
+  //     method: "POST",
+  //     body: JSON.stringify(postData),
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   });
+  //   setPosts((existingPosts) => [postData, ...existingPosts]);
+  // }
 
   return (
     <>
-      {/*{isPosting && (<Modal onClose={onStopPosting}>*/}
-      {/*  <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>*/}
-      {/*</Modal>)}*/}
       {posts.length > 0 && <ul className={classes.posts}>
         {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
       </ul>}
@@ -30,7 +27,6 @@ function PostsList() {
         <h2> There are no posts yet!</h2>
         <p> Start adding some!</p>
       </div>)}
-      {/*{isFetching && (<p style={{textAlign: "center", color: "white"}}>Loading posts ...</p>) }*/}
 
     </>
   );
--- react/my-vite-app/src/main.jsx
+++ react/my-vite-app/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 // import App from "./App.jsx";
-import NewPost from "./routes/NewPost.jsx";
+import NewPost, {action as newPostAction} from "./routes/NewPost.jsx";
 import RootLayout from "./routes/RootLayout.jsx";
 import Posts, {loader as postsLoader} from "./routes/Posts.jsx";
 
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     path: "/", element: <RootLayout/>, children: [
       {
         path: "/", element: <Posts/>, loader: postsLoader, children: [
-          {path: "/create-post", element: <NewPost/>},
+          {path: "/create-post", element: <NewPost/>, action: newPostAction},
         ]
       },
 
