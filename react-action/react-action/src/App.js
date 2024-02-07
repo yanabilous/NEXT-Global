54b9ba5cac652ea4e095e9d0dde4359ff78133ed
yanabilous <yanabilous05@gmail.com>
2024-02-07 13:30:16 +0200

    fix

--- react-action/react-action/src/App.js
+++ react-action/react-action/src/App.js
@@ -1,13 +1,13 @@
-import Todo from "./components/Todo";
+// import Todo from "./components/Todo";
 
 
 function App() {
   return (
     <div>
-      <h1>My todos</h1>
-      <Todo text="text 1"/>
-      <Todo text="text 2"/>
-      <Todo text="text 3"/>
+      {/*<h1>My todos</h1>*/}
+      {/*<Todo text="text 1"/>*/}
+      {/*<Todo text="text 2"/>*/}
+      {/*<Todo text="text 3"/>*/}
 
     </div>
   );
