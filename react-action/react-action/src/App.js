2d862b1f052cb5caec3efd777869dda2dc2dcdc2
yanabilous <yanabilous05@gmail.com>
2024-02-07 20:46:44 +0200

    added layout

--- react-action/react-action/src/App.js
+++ react-action/react-action/src/App.js
@@ -1,38 +1,21 @@
-//
-// import {Route, Switch} from "react-router-dom";
-// import AllMeetupsPage from "./pages/AllMeetups";
-// import NewMeetupPage from "./pages/NewMeetup";
-// import FavoritesPage from "./pages/Favorites";
-//
-// function App() {
-//   return (
-//     <div>
-//       <Switch>
-//         <Route path="/"><AllMeetupsPage/></Route>
-//         <Route path="/new-meetup"><NewMeetupPage/></Route>
-//         <Route path="/favorites"><FavoritesPage/></Route>
-//       </Switch>
-//     </div>
-//   );
-// }
-//
-// export default App;
+
 import {Routes, Route} from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+
 
 function App() {
   return (
-    <>
-      <MainNavigation/>
+    <Layout>
+
       <Routes>
         <Route path="/" element={<AllMeetupsPage/>}/>
         <Route path="/new-meetup" element={<NewMeetupPage/>}/>
         <Route path="/favorites" element={<FavoritesPage/>}/>
       </Routes>
-    </>
+    </Layout>
   );
 }
 
