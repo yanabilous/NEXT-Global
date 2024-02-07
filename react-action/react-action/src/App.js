c5da0b06b5fdafbf8116479787320e3150c6572b
yanabilous <yanabilous05@gmail.com>
2024-02-07 18:11:40 +0200

    added links and naw

--- react-action/react-action/src/App.js
+++ react-action/react-action/src/App.js
@@ -21,14 +21,18 @@ import {Routes, Route} from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AllMeetupsPage />} />
-      <Route path="/new-meetup" element={<NewMeetupPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-    </Routes>
+    <>
+      <MainNavigation/>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage/>}/>
+        <Route path="/new-meetup" element={<NewMeetupPage/>}/>
+        <Route path="/favorites" element={<FavoritesPage/>}/>
+      </Routes>
+    </>
   );
 }
 
