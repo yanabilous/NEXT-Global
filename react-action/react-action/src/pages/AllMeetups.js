64a4f7e69d390ce2249ccc1b3ded75d13fd943d1
yanabilous <yanabilous05@gmail.com>
2024-02-08 14:35:53 +0200

    useEffect

--- react-action/react-action/src/pages/AllMeetups.js
+++ react-action/react-action/src/pages/AllMeetups.js
@@ -1,5 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 // const DUMMY_DATA = [
 //   {
@@ -25,13 +25,27 @@ import {useState} from "react";
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
-  fetch("https://react-getting-started-1061c-default-rtdb.firebaseio.com/meetups.json"
-  ).then(response => {
-    return response.json();
-  }).then(data => {
-    setIsLoading(false);
-    setLoadedMeetups(data);
-  });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://react-getting-started-1061c-default-rtdb.firebaseio.com/meetups.json"
+    ).then(response => {
+      return response.json();
+    }).then(data => {
+      const meetups = [];
+
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        };
+        meetups.push(meetup);
+      }
+
+      setIsLoading(false);
+      setLoadedMeetups(meetups);
+    });
+  }, []);
+
   if (isLoading) {
     return (
       <section>
