5a4c4ad6a0a51ddf0d71c17c43f894dd66693d3a
yanabilous <yanabilous05@gmail.com>
2024-02-26 11:42:49 +0200

    fix

--- next-eatery/app/not-found.js
+++ next-eatery/app/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return <main className="not-found">
-    <h1>Nit found</h1>
-    <p>Unfortunately, we could not find the requested page or resource.</p>
+    <h1>Meal not found</h1>
+    <p>Unfortunately, we could not find the requested page or meal data.</p>
   </main>;
 }
