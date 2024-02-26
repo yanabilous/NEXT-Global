8595198a6fa8f82f8c2ef771b6ed93f81fc096ff
yanabilous <yanabilous05@gmail.com>
2024-02-26 11:27:12 +0200

    added files

--- next-eatery/app/meals/[mealSlug]/page.js
+++ next-eatery/app/meals/[mealSlug]/page.js
@@ -1,16 +1,27 @@
 import Image from "next/image";
+import {getMeal} from "@/lib/meals";
 import classes from "./page.module.css";
 
 
-export default function MealDetailsPage() {
+
+export default function MealDetailsPage({params}) {
+  const meal = getMeal(params.mealSlug);
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image/>
+          <Image fill src={meal.image} alt={meal.title}/>
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}> by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}></p>
 
       </main>
     </>
