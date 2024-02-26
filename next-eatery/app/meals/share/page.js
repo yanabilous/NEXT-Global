d74058df9e1a5b4ee42d080f7c7d0b264f55e071
yanabilous <yanabilous05@gmail.com>
2024-02-26 14:18:10 +0200

    fix

--- next-eatery/app/meals/share/page.js
+++ next-eatery/app/meals/share/page.js
@@ -1,7 +1,21 @@
-import classes from './page.module.css';
+import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email")
+    };
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,24 +25,24 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" required/>
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" required/>
             </p>
           </div>
           <p>
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <input type="text" id="title" name="title" required/>
           </p>
           <p>
             <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <input type="text" id="summary" name="summary" required/>
           </p>
           <p>
             <label htmlFor="instructions">Instructions</label>
@@ -39,7 +53,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker/>
+          <ImagePicker label="Your image" name="image"/>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
