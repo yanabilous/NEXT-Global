fb18441c3222b6013a5af365aae34a31cdd1dcf5
yanabilous <yanabilous05@gmail.com>
2024-02-27 17:38:03 +0200

    router

---
+++ next-blog/pages/about/index.js
@@ -0,0 +1,7 @@
+function AboutPage() {
+  return <div>
+    <h1>The About Page</h1>
+  </div>;
+}
+
+export default AboutPage;
---
+++ next-blog/pages/clients/index.js
@@ -0,0 +1,10 @@
+function ClientsPage() {
+
+
+  return <div>
+    <h1>The Clients Page</h1>
+
+  </div>;
+}
+
+export default ClientsPage;
