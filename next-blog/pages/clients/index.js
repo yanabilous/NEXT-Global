c171b505632242f04a9ac1a5108e39b5ce89db99
yanabilous <yanabilous05@gmail.com>
2024-02-27 18:24:23 +0200

    fix syntax

--- next-blog/pages/clients/index.js
+++ next-blog/pages/clients/index.js
@@ -1,16 +1,28 @@
 import Link from "next/link";
 
 function ClientsPage() {
-const clients = [
-  {id: "yana", name: "Yana"},
-  {id: "menu", name: "menu"},
-]
+  const clients = [
+    {id: "yana", name: "Yana"},
+    {id: "menu", name: "menu"},
+  ];
 
   return <div>
     <h1>The Clients Page</h1>
-    <ul>
-      {clients.map((client => <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link></li>))}
+    {/*<ul>*/}
+    {/*  {clients.map((client => <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link></li>))}*/}
 
+    {/*</ul>*/}
+    <ul>
+      {clients.map((client => <li key={client.id}>
+        <Link
+          href={{
+            pathname: "/clients/[id]",
+            query: {id: client.id}
+          }}
+        >
+          {client.name}
+        </Link>
+      </li>))}
     </ul>
 
   </div>;
