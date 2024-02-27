c38d955b1611edfd63a425626aaabc9a4fe638ff
yanabilous <yanabilous05@gmail.com>
2024-02-27 18:32:51 +0200

    added router

--- next-blog/pages/clients/[id]/index.js
+++ next-blog/pages/clients/[id]/index.js
@@ -5,8 +5,18 @@ function ClientProjectsPage() {
  const router =useRouter();
 
  console.log(router.query);
+ function loadProjectHandler(){
+   router.push({
+     pathname: '/clients/[id]/[clientprojectid]',
+     query: {id: "yana", clientprojectid: "projecta"}
+   })
+
+ }
+
+
   return <div>
     <h1>The Client Projects Page</h1>
+    <button onClick={loadProjectHandler}>Load Project A</button>
 
   </div>;
 }
