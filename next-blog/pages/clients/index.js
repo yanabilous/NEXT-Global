2a0e5ab41949aa4d347f06ba106b5f3a85bdc32b
yanabilous <yanabilous05@gmail.com>
2024-02-27 18:17:10 +0200

    router

--- next-blog/pages/clients/index.js
+++ next-blog/pages/clients/index.js
@@ -1,8 +1,17 @@
-function ClientsPage() {
+import Link from "next/link";
 
+function ClientsPage() {
+const clients = [
+  {id: "yana", name: "Yana"},
+  {id: "menu", name: "menu"},
+]
 
   return <div>
     <h1>The Clients Page</h1>
+    <ul>
+      {clients.map((client => <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link></li>))}
+
+    </ul>
 
   </div>;
 }
--- next-blog/pages/index.js
+++ next-blog/pages/index.js
@@ -1,6 +1,22 @@
+import Link from "next/link";
+
 function HomePage() {
   return <div>
     <h1>The Home Page</h1>
+    <ul>
+      <li>
+        <Link href="/portfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link href="/clients">clients</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+      <li>
+        <Link href="/blog/[...slug]">Blog</Link>
+      </li>
+    </ul>
   </div>;
 }
 
