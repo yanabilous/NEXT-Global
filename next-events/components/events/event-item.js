c72fae954f8360ac1376d7396b3e8a2164c98721
yanabilous <yanabilous05@gmail.com>
2024-02-28 20:20:32 +0200

    added styles

--- next-events/components/events/event-item.js
+++ next-events/components/events/event-item.js
@@ -1,33 +1,35 @@
 import Link from "next/link";
 
+import classes from "./event-item.module.css";
+
+
 function EventItem(props) {
   const {title, image, date, location, id} = props;
-  const humanReadableDate =new Date(date).toLocaleDateString("en-US", {
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
     year: "numeric",
-  })
+  });
 
-  const formattedAddress = location.replace(', ', '\n');
+  const formattedAddress = location.replace(", ", "\n");
 
-  const exploreLink = `/events/${id}`
+  const exploreLink = `/events/${id}`;
 
 
-
-  return <li>
-    <img src={ '/' +  image} alt={title }/>
-    <div>
-      <div>
+  return <li className={classes.item}>
+    <img src={"/" + image} alt={title}/>
+    <div className={classes.content}>
+      <div className={classes.summary}>
         <h2>{title}</h2>
       </div>
-      <div>
+      <div className={classes.date}>
         <time>{humanReadableDate}</time>
       </div>
-      <div>
+      <div className={classes.address}>
         <address>{formattedAddress}</address>
       </div>
     </div>
-    <div>
+    <div className={classes.actions}>
       <Link href={exploreLink}>Explore Event</Link>
     </div>
   </li>;
