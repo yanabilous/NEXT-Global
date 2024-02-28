604b440d6fefcc3ac3e044c3e239c38340490bf7
yanabilous <yanabilous05@gmail.com>
2024-02-28 21:55:47 +0200

    fix

--- next-events/components/events/event-item.js
+++ next-events/components/events/event-item.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
 import classes from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 
 function EventItem(props) {
@@ -23,14 +27,19 @@ function EventItem(props) {
         <h2>{title}</h2>
       </div>
       <div className={classes.date}>
+        <DateIcon/>
         <time>{humanReadableDate}</time>
       </div>
       <div className={classes.address}>
+        <AddressIcon/>
         <address>{formattedAddress}</address>
       </div>
     </div>
     <div className={classes.actions}>
-      <Link href={exploreLink}>Explore Event</Link>
+      <Button link={exploreLink}>
+       <span>Explore Event</span>
+       <span className={classes.icon}><ArrowRightIcon/></span>
+      </Button>
     </div>
   </li>;
 }
