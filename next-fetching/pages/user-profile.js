94c6e0a64a2cb08a74b965a618e4d2677d5fe2aa
yanabilous <yanabilous05@gmail.com>
2024-03-06 19:37:47 +0200

    added

---
+++ next-fetching/pages/user-profile.js
@@ -0,0 +1,14 @@
+function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      username: "Yana"
+    }
+  };
+}
