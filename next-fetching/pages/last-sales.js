478c6635be7245c7beba589f12c5814ac0322a3c
yanabilous <yanabilous05@gmail.com>
2024-03-06 23:47:40 +0200

    added

--- next-fetching/pages/last-sales.js
+++ next-fetching/pages/last-sales.js
@@ -1,34 +1,52 @@
 import {useEffect, useState} from "react";
+import useSWR from 'swr';
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const {data, error} = useSWR("https://next-course1-2a13c-default-rtdb.firebaseio.com/sales.json",);
   useEffect(() => {
-    setIsLoading(true)
-    fetch("https://next-course1-2a13c-default-rtdb.firebaseio.com/sales.json"
-    ).then((response) => response.json())
-      .then(data => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({id: key, username: data[key].username, volume: data[key].volume })
-        }
+      for (const key in data) {
+        transformedSales.push({id: key, username: data[key].username, volume: data[key].volume});
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true)
+  //   fetch("https://next-course1-2a13c-default-rtdb.firebaseio.com/sales.json")
+  //     .then((response) => response.json())
+  //     .then(data => {
+  //       const transformedSales = [];
+  //
+  //       for (const key in data) {
+  //         transformedSales.push({id: key, username: data[key].username, volume: data[key].volume })
+  //       }
+  //
+  //
+  //
+  //       setSales(transformedSales)
+  //       setIsLoading(false)
+  //     })
+  // }, []);
 
 
-
-        setSales(transformedSales)
-        setIsLoading(false)
-      })
-  }, []);
-
-  if(isLoading) {
-    return <p>Loading...</p>
+  if (error) {
+    return <p>Failed to load...</p>;
   }
 
-  if(!sales){
-    return <p>No data yet...</p>
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
+
+
+
+
   return <ul>
     {sales.map((sale) => (
       <li key={sale.id}>{sale.username} - ${sale.volume}</li>
