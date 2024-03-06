2ab9c90324a31b9fe4a489a1c96a1630dbcbda95
yanabilous <yanabilous05@gmail.com>
2024-03-06 18:33:17 +0200

    added

--- next-fetching/pages/[pid].js
+++ next-fetching/pages/[pid].js
@@ -1,9 +1,13 @@
-import { Fragment } from "react";
+import {Fragment} from "react";
 import path from "path";
 import fs from "fs/promises";
 
 function ProductDetailPage(props) {
-  const { loadedProduct } = props;
+  const {loadedProduct} = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Fragment>
@@ -14,12 +18,16 @@ function ProductDetailPage(props) {
 }
 
 export async function getStaticProps(context) {
-  const { params } = context;
+  const {params} = context;
   const productId = params.pid;
 
-  const data = await getData(); // Тепер отримуємо дані на сервері
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return {notFound: true};
+  }
 
   return {
     props: {
@@ -29,13 +37,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const data = await getData(); // Тепер отримуємо дані на сервері
+  const data = await getData();
   const ids = data.products.map((product) => product.id);
-  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+  const pathsWithParams = ids.map((id) => ({params: {pid: id}}));
 
   return {
     paths: pathsWithParams,
-    fallback: false,
+    fallback: true,
   };
 }
 
