5703410dd28cf5e60026728c0f7a322bd0605c00
yanabilous <yanabilous05@gmail.com>
2024-03-26 19:14:32 +0200

    added

--- next-events/pages/api/newsletter.js
+++ next-events/pages/api/newsletter.js
@@ -1,14 +1,20 @@
-function handler(req, res) {
+import {MongoClient} from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
-    if(!userEmail || !userEmail.includes("@")){
-      res.status(422).json({message: "Invalid email address"})
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({message: "Invalid email address"});
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({message: "Signed up!"})
+    const client = await MongoClient.connect("mongodb+srv://yanabilous05:0HJyI2xPlMD7cce5@cluster0.txcaw1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+    const db = client.db();
+    await db.collection("emails").insertOne({email: userEmail});
+    client.close();
+    res.status(201).json({message: "Signed up!"});
   }
 }
 
