7eb57e4705df4f174e9f905bdb1fc8cbf6f565b2
yanabilous <yanabilous05@gmail.com>
2024-03-28 12:59:57 +0200

    added

--- next-events/store/notification-context.js
+++ next-events/store/notification-context.js
@@ -1,16 +1,27 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 const NotificationContext = createContext({
   notification: null,
-  showNotification: function () {
+  showNotification: function (notificationData) {
   },
   hideNotification: function () {
   }
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+  setActiveNotification(null);
+  }
+
+  const context = {notification: activeNotification, showNotification: showNotificationHandler, hideNotification: hideNotificationHandler}
+
   return (
-    <NotificationContextProvider>
+    <NotificationContextProvider value={context}>
       {props.children}
     </NotificationContextProvider>
   );
