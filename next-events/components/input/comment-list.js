15f54f322fc44329eef7dbe3dc948a6204248f8d
yanabilous <yanabilous05@gmail.com>
2024-03-28 16:18:15 +0200

    added

--- next-events/components/input/comment-list.js
+++ next-events/components/input/comment-list.js
@@ -5,7 +5,7 @@ function CommentList(props) {
 
   return (
     <ul className={classes.comments}>
-      {/* Render list of comments - fetched from API */}
+
       {items.map(item =>
         <li key={item._id}>
           <p>{item.text}</p>
--- next-events/components/input/newsletter-registration.js
+++ next-events/components/input/newsletter-registration.js
@@ -1,16 +1,22 @@
-import classes from './newsletter-registration.module.css';
-import {useRef} from "react";
+import classes from "./newsletter-registration.module.css";
+import {useContext, useRef} from "react";
+import NotificationContext from "../../store/notification-context";
+import {error} from "next/dist/build/output/log";
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
+
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
 
-    const enteredEmail = emailInputRef.current.value
+    notificationCtx.showNotification({
+      title: "Signing up...",
+      message: "Registering for newsletter",
+      status: "pending"
+    });
 
     fetch("/api/newsletter", {
       method: "POST",
@@ -18,8 +24,30 @@ function NewsletterRegistration() {
       headers: {
         "Content-Type": "application/json",
       }
-    }).then(response => response.json())
-      .then((data) => console.log(data))
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return response.json().then(data => {
+        throw new Error(data.message || "Something went wrong");
+      });
+    })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Successfully registered for newsletter",
+          status: "success"
+        });
+      })
+      .catch(error => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong",
+          status: "error"
+        });
+      });
+
   }
 
   return (
@@ -28,10 +56,10 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
             ref={emailInputRef}
           />
           <button>Register</button>
