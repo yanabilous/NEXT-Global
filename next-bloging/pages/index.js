6b3ce73b6308725d34976952df53e85584261e7e
yanabilous <yanabilous05@gmail.com>
2024-04-02 22:31:50 +0300

    added

--- next-bloging/pages/index.js
+++ next-bloging/pages/index.js
@@ -1,4 +1,14 @@
+import {Fragment} from "react";
+import Hero from "../components/home-page/hero";
+
 function HomePage() {
+
+  return(
+    <Fragment>
+      <Hero />
+      {/*<FeauteredPosts />*/}
+    </Fragment>
+  )
 
 }
 
