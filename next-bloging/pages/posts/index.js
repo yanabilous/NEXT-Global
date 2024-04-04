323f73d2364dab3ee3c430dcbc018bf078c308cb
yanabilous <yanabilous05@gmail.com>
2024-04-04 11:29:37 +0300

    added

--- next-bloging/pages/posts/index.js
+++ next-bloging/pages/posts/index.js
@@ -1,4 +1,39 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
+
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started With Next1",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for production",
+    date: "2024-03-10"
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started With Next2",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for production",
+    date: "2024-03-10"
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started With Next3",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for production",
+    date: "2024-03-10"
+  },
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started With Next4",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a the React framework for production",
+    date: "2024-03-10"
+  },
+];
+
+function AllPostsPage(props) {
+  return <AllPosts posts={DUMMY_POSTS}/>
 
 }
 
