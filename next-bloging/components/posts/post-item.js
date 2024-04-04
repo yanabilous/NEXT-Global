8528625730997aa43dadb80c7b8b0af67555dcef
yanabilous <yanabilous05@gmail.com>
2024-04-04 11:18:14 +0300

    added

--- next-bloging/components/posts/post-item.js
+++ next-bloging/components/posts/post-item.js
@@ -10,14 +10,14 @@ function PostItem(props) {
     year: "numeric"
   });
 
-  const imagePath = `/image/posts/${slug}/${image}`;
+  const imagePath = `/images/posts/${slug}/${image}`;
   const linkPath = `/posts/${slug}`;
 
   return <li className={classes.post}>
     <Link href={linkPath}>
       {/*<a>*/}
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200}/>
+          <Image src={imagePath} alt={title} width={300} height={200} layout="responsive"/>
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
--- next-bloging/pages/index.js
+++ next-bloging/pages/index.js
@@ -5,28 +5,28 @@ import FeaturedPosts from "../components/home-page/featured-posts";
 
 const DUMMY_POSTS = [
   {
-    slug: "getting-started-with-next1",
+    slug: "getting-started-with-nextjs",
     title: "Getting Started With Next1",
     image: "getting-started-nextjs.png",
     excerpt: "NextJS is a the React framework for production",
     date: "2024-03-10"
   },
   {
-    slug: "getting-started-with-next2",
+    slug: "getting-started-with-nextjs2",
     title: "Getting Started With Next2",
     image: "getting-started-nextjs.png",
     excerpt: "NextJS is a the React framework for production",
     date: "2024-03-10"
   },
   {
-    slug: "getting-started-with-next3",
+    slug: "getting-started-with-nextjs3",
     title: "Getting Started With Next3",
     image: "getting-started-nextjs.png",
     excerpt: "NextJS is a the React framework for production",
     date: "2024-03-10"
   },
   {
-    slug: "getting-started-with-next4",
+    slug: "getting-started-with-nextjs",
     title: "Getting Started With Next4",
     image: "getting-started-nextjs.png",
     excerpt: "NextJS is a the React framework for production",
