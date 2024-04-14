4b9420d02d7c3efd01b381516b16600077018954
yanabilous <yanabilous05@gmail.com>
2024-04-14 17:17:07 +0300

    added

--- next-bloging/components/posts/post-detail/post-content.js
+++ next-bloging/components/posts/post-detail/post-content.js
@@ -3,16 +3,22 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import {PrismLight as SyntaxHighlighter} from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 function PostContent(props) {
-  const { post } = props;
+  const {post} = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
     p(paragraph) {
-      const { node } = paragraph;
+      const {node} = paragraph;
 
       if (node.children[0].tagName === "img") {
         const image = node.children[0];
@@ -32,7 +38,7 @@ function PostContent(props) {
       return <p>{paragraph.children}</p>;
     },
     code(code) {
-      const { className, children } = code;
+      const {className, children} = code;
       const language = className.split("-")[1];
       return (
         <SyntaxHighlighter style={atomDark} language={language}>
@@ -44,7 +50,7 @@ function PostContent(props) {
 
   return (
     <article className={classes.content}>
-      <PostHeader title={post.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath}/>
       <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
