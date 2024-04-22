7488918ce956e3e70af0ce874f3e355f1d681dde
yanabilous <yanabilous05@gmail.com>
2024-04-22 18:50:25 +0300

    added

--- next-authentication/components/profile/user-profile.js
+++ next-authentication/components/profile/user-profile.js
@@ -1,36 +1,94 @@
-import ProfileForm from "./profile-form";
-// import { getSession } from "next-auth/client";
-import classes from "./user-profile.module.css";
-// import {useEffect, useState} from "react";
+// import ProfileForm from "./profile-form";
+// // import { getSession } from "next-auth/client";
+// import classes from "./user-profile.module.css";
+//
+// // import {useEffect, useState} from "react";
+//
+// function UserProfile() {
+//   // const [isLoading, setIsLoading] = useState(true);
+//   // // const [loadedSession, setLoadedSession] = useState();
+//   // //   const [session, loading] = useSession();
+//   //
+//   // useEffect(() => {
+//   //   getSession().then(session => {
+//   //
+//   //     if (!session) {
+//   //       window.location.href = "/auth";
+//   //     } else {
+//   //       setIsLoading(false);
+//   //     }
+//   //     // setLoadedSession(session);
+//   //
+//   //   });
+//   // }, []);
+//   //
+//   //
+//   // // Redirect away if NOT auth
+//   // if (isLoading) {
+//   //   return <p className={classes.profile}>Loading ... </p>;
+//   // }
+//
+//   async function changePasswordHandler(passwordData) {
+//     const response = await fetch("/api/user/change-password", {
+//       method: "PATCH",
+//       body: JSON.stringify(passwordData),
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//
+//   }
+//
+//   return (
+//     <section className={classes.profile}>
+//       <h1>Your User Profile</h1>
+//       <ProfileForm onChengePassword={changePasswordHandler}/>
+//     </section>
+//   );
+// }
+//
+// export default UserProfile;
+
+import ProfileForm from './profile-form';
+import classes from './user-profile.module.css';
 
 function UserProfile() {
   // const [isLoading, setIsLoading] = useState(true);
-  // // const [loadedSession, setLoadedSession] = useState();
-  // //   const [session, loading] = useSession();
-  //
+
   // useEffect(() => {
-  //   getSession().then(session => {
-  //
+  //   getSession().then((session) => {
   //     if (!session) {
-  //       window.location.href = "/auth";
+  //       window.location.href = '/auth';
   //     } else {
   //       setIsLoading(false);
   //     }
-  //     // setLoadedSession(session);
-  //
   //   });
   // }, []);
-  //
-  //
-  // // Redirect away if NOT auth
+
   // if (isLoading) {
-  //   return <p className={classes.profile}>Loading ... </p>;
+  //   return <p className={classes.profile}>Loading...</p>;
   // }
+
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
 
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm/>
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
