5d2e3d7c2ec9355abe00dc2805f1cc4abe85996d
yanabilous <yanabilous05@gmail.com>
2024-04-29 23:41:29 +0300

    init

---
+++ summ/components/meetups/MeetupDetail.js
@@ -0,0 +1,25 @@
+import classes from './MeetupDetail.module.css';
+
+function MeetupDetail(props) {
+  return (
+    <section className={classes.detail}>
+      <img
+        src={props.image}
+        alt={props.title}
+      />
+      <h1>{props.title}</h1>
+      <address>{props.address}</address>
+      <p>{props.description}</p>
+    </section>
+  );
+}
+
+export default MeetupDetail;
+
+ // id: "m1",
+//     title: "This is a first meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
---
+++ summ/pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://yanabilous05:c02y7hqdLz1TJbhY@cluster0.medjx1s.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
